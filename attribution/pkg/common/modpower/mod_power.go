4e08d4cb59e061fa908ce3a1dd6c02134dfc7a01
attribution <linceyou@tencent.com>
2020-10-23 15:48:26 +0800

    add code

---
+++ attribution/pkg/common/modpower/mod_power.go
@@ -0,0 +1,38 @@
+/*
+ * copyright (c) 2020, Tencent Inc.
+ * All rights reserved.
+ *
+ * Author:  linceyou@tencent.com
+ * Last Modify: 10/23/20, 11:39 AM
+ */
+
+package modpower
+
+import "math/big"
+
+type ModPower struct {
+	Prime  *big.Int
+	EncKey *big.Int
+	DecKey *big.Int
+}
+
+func NewModPower(prime *big.Int, encKey *big.Int) *ModPower {
+	e := &ModPower{
+		Prime:  prime,
+		EncKey: encKey,
+	}
+	e.DecKey = e.findDecKey()
+	return e
+}
+
+func (e *ModPower) Encrypt(data *big.Int) *big.Int {
+	return big.NewInt(0).Exp(data, e.EncKey, e.Prime)
+}
+
+func (e *ModPower) Decrypt(data *big.Int) *big.Int {
+	return big.NewInt(0).Exp(data, e.DecKey, e.Prime)
+}
+
+func (e *ModPower) findDecKey() *big.Int {
+	return big.NewInt(0).ModInverse(e.EncKey, big.NewInt(0).Sub(e.Prime, big.NewInt(1)))
+}
--- attribution/pkg/leads/pull/leads_pull.go
+++ attribution/pkg/leads/pull/leads_pull.go
@@ -8,6 +8,40 @@
 
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/TencentAd/attribution/attribution/pkg/common/flagx"
+	"github.com/TencentAd/attribution/attribution/pkg/leads/pull/client"
+	"github.com/TencentAd/attribution/attribution/pkg/storage"
+)
+
+var (
+	leadsPullConfig = flag.String("leads_pull_config", "", "")
+)
+
+func run() error {
+	pullConfig, err := client.NewPullConfig(*leadsPullConfig)
+	if err != nil {
+		return err
+	}
+
+	leadsStorage, err := storage.CreateLeadsStorage()
+	if err != nil {
+		return err
+	}
+
+	pullClient := client.NewLeadsPullClient(pullConfig).WithStorage(leadsStorage)
+	return pullClient.Pull()
+}
+
+func main() {
+	err := flagx.Parse()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := run(); err != nil {
+		panic(err)
+	}
 }
